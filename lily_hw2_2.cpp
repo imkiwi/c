309767c9a0b8f5ac522e2e65b8333fb6ce248262
imkiwi <42855934+imkiwi@users.noreply.github.com>
2018-09-06 17:53:43 +0800

    234

--- lily_hw2_2.cpp
+++ lily_hw2_2.cpp
@@ -2,42 +2,35 @@
 using namespace std;
 int  main()
 {
-	double n[10]={-1000e28,-1000e28,-1000e28,-1000e28,-1000e28,-1000e28,-1000e28,-1000e28,-1000e28,-1000e28};
-	
-	cout << "Please input 10 numbers." << endl;
-	int i,j;
-	double t;
-	for(i=0 ; i<10 ; i++)
-	{
-		cin >> n[i]; 	
-	}
-	cout << endl;
+    double defaultValue = -1000e28;
+    double n[10]={defaultValue};
+    int arraySize = 10;
+    cout << "Please input 10 numbers." << endl;
+    int i,j;
+    double t;
+    for(i=0 ; i<arraySize ; i++)
+    {
+        cin >> n[i];
+    }
+    cout << endl;
+    
+    // bubble sort
+    for(i=1 ; i<arraySize ; i++)
+    {
+        for(j=arraySize-1 ; j >= i ;j--)
+        {
+            if(n[j-1] > n[j])
+            {
+                t = n[j-1];
+                n[j-1] = n[j];
+                n[j] = t;
+            }
+        }
+    }
+    
+    cout << "top three numbers  ";
+    cout << n[9] << "  " << n[8] << "  " << n[7] <<endl;
 
-	
-	for(i=1 ; i<10 ; i++)
-	{
-		for(j=10-1 ; j >= i ;j--)
-		{
-			if(n[j-1]>n[j])
-			{
-				t=n[j-1];
-				n[j-1]=n[j];
-				n[j]=t;
-				
-			}
-		}
-	
-	
-	}
-	/*for(int i=0 ; i<5 ;i++)
-	{
-		cout << n[i] << endl;
-	}*/
-
-	cout << "top three numbers  ";
-	cout << n[9] << "  " << n[8] << "  " << n[7] <<endl;
-
-	
-	system("pause");
-	return 0;
+    system("pause");
+    return 0;
 }
